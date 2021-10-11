ec859f5a0fd273241961f1844e9f88d235877769
AlexandreSGJR <alexandre.guedesjr@bandtec.com.br>
2021-10-11 16:22:28 -0300

    feat:add information and reports to home

---
+++ src/components/Carousel/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import './carousel';
+import './style.css';
+import img from '../../img/option-tattoo.jpg';
+
+function Carousel(props) {
+    return (
+        <>
+            <div className="items-wrapper">
+                <div id="items">
+                    <div className="item">
+                        <div className="report">
+                            <img src={img} alt="Foto usuário" />
+                            <div>
+                                <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti esse labore expedita officiis eum provident, nemo ab."</p>
+                                <span>Nome do usário</span>
+                            </div>
+                        </div>
+                        {/* <img src={img} alt="" /> */}
+                    </div>
+                    <div className="item">
+                        <div className="report">
+                            <img src={img} alt="Foto usuário" />
+                            <div>
+                                <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti esse labore expedita officiis eum provident, nemo ab."</p>
+                                <span>Nome do usário</span>
+                            </div>
+                        </div>
+                        {/* <img src={img} alt="" /> */}
+                    </div>
+                    <div className="item">
+                        <div className="report">
+                            <img src={img} alt="Foto usuário" />
+                            <div>
+                                <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti esse labore expedita officiis eum provident, nemo ab."</p>
+                                <span>Nome do usário</span>
+                            </div>
+                        </div>
+                        {/* <img src={img} alt="" /> */}
+                    </div>
+                    <div className="item">
+                        <div className="report">
+                            <img src={img} alt="Foto usuário" />
+                            <div>
+                                <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti esse labore expedita officiis eum provident, nemo ab."</p>
+                                <span>Nome do usário</span>
+                            </div>
+                        </div>
+                        {/* <img src={img} alt="" /> */}
+                    </div>
+                    <div className="item">
+                        <div className="report">
+                            <img src={img} alt="Foto usuário" />
+                            <div>
+                                <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deleniti esse labore expedita officiis eum provident, nemo ab."</p>
+                                <span>Nome do usário</span>
+                            </div>
+                        </div>
+                        {/* <img src={img} alt="" /> */}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Carousel;
