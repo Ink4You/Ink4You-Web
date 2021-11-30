840b2f867892ed4acffdc5766837cfd050f7dabb
unknown <davidrafaelmsm3@gmail.com>
2021-11-29 23:22:58 -0300

    feat: input tatuadores

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,49 +1,92 @@
-import React, { useEffect, useState } from "react";
-import './style.css';
-import img from '../../img/option-tattoo.jpg';
-import api from '../../api';
+import React, { useState } from "react";
+import Header from "../../components/Header";
+import Filters from "../../components/Filters";
+import SearchInput from "../../components/SearchInput";
+import Flatlist from "../../components/Flatlist";
+import { testeCard } from "../../utils/MockData";
+import api from "../../api";
+import "./style.css";
 
+function TattooList() {
+  const [tattooList, setTattooList] = useState([]);
 
-function Carousel() {
-    const [relatos, setRelatos] = useState([]);
+  async function GetTattooList() {
+    const { data } = await api.get("/tatuagens");
+  }
 
+  function ChangeImage(e) {
+    let file = e.target.files[0];
+    let formData = new FormData();
+    formData.append("file", file)
 
-    useEffect(() => {
-        async function getData() {
-            const { data } = await api.get("/relatos/buscar-relatos");
-            setRelatos(data.pilha);
-        }
-        getData();
-    }, []);
-    return (
-        <>
-            <div className="items-wrapper">
-                <div id="items">
-                    <div className="item">
+    api.patch("tatuadores/foto/8", formData, {
+      headers: {
+        "Content-type": "multipart/form-data",
+      },
+    });
 
-                        <div className="report">
-                            {
-                                relatos?.map((element) =>
-                                    element !== null && (
-                                        <>
-                                            <img src={element?.imagem || ""} />
-                                            <div>
-                                                <p>{element?.descricao || ""}</p>
-                                                <span>{element?.nome_usuario || ""}</span>
-                                            </div>
-                                        </>
-                                    )
+    // let reader = new FileReader();
+    // reader.readAsDataURL(files[0]);
 
-                                )
+    // reader.onload = (e) => {
+    //   let result = e.target.result;
+    //   let bytes = result.split(",")[1];
 
-                            }
-                        </div>
-                    </div>
+    //   let formData = new FormData();
+    //   let imageFile = e.target.result;
+    //   console.log("imageFile", imageFile);
 
-                </div>
-            </div>
-        </>
-    );
+    //   formData.append("file", imageFile);
+
+    //   console.log(formData);
+    //   api.patch("tatuadores/foto/8",formData, {
+    //       headers: {
+    //           "Content-type" : "multipart/form-data",
+    //       },
+    //   });
+
+    //   console.log("bytes :", bytes);
+    //   api.patch("tatuadores/foto/8", { foto: bytes }).then((response) => {
+    //     console.log(response);
+    //   });
+    // };
+  }
+  //   var formData = new FormData();
+  //   var imagefile = document.querySelector("#file");
+  //   formData.append("image", imagefile.files[0]);
+  //   axios.post("upload_file", formData, {
+  //     headers: {
+  //       "Content-Type": "multipart/form-data",
+  //     },
+  //   });
+
+  return (
+    <>
+      <Header />
+      <section className="container tattoo-list-container">
+        <Filters
+          StyleEnabled={true}
+          LocalEnabled={true}
+          LocalizationEnabled={true}
+        />
+        <section className="tattoo-list">
+          <div className="header">
+            <h2>Tatuagens</h2>
+            <SearchInput />
+          </div>
+          <div className="content">
+            <Flatlist data={testeCard} type="tattoo" wrap={true} />
+          </div>
+          <input
+            type="file"
+            name="file"
+            accept="image/*"
+            onChange={(e) => ChangeImage(e)}
+          />
+        </section>
+      </section>
+    </>
+  );
 }
 
-export default Carousel;
+export default TattooList;
